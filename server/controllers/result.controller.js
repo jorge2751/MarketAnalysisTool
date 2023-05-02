ccefb052702b1055d1ee81f5e19a71e030a53793
Jorge Leon <84067777+jorge2751@users.noreply.github.com>
2023-05-01 20:36:25 -0700

    Updates

--- server/controllers/result.controller.js
+++ server/controllers/result.controller.js
@@ -1,9 +1,8 @@
 const Result = require('../models/result.model');
-const { City } = require('../models/city.model');
-const { State } = require('../models/state.model');
+const City = require('../models/city.model');
+const State = require('../models/state.model');
 const { Op } = require("sequelize");
-const { spawn } = require('child_process');
-const path = require('path');
+const { PythonShell } = require('python-shell');
 
 exports.getAllResults = async (req, res) => {
     try {
@@ -39,7 +38,13 @@ exports.getResultById = async (req, res) => {
 exports.createResult = async (req, res) => {
     try {
         const { niche, state_id, min, max } = req.body;
+        console.log(niche, state_id, min, max)
+
         const state = await State.findByPk(state_id);
+
+        if (!state) {
+            return res.status(404).json({ error: 'State not found' });
+        }
 
         // get cities by state and population range
         const cities = await City.findAll({
@@ -51,23 +56,32 @@ exports.createResult = async (req, res) => {
             }
         });
 
-        // Define path to python script
-        const scriptPath = path.join(__dirname, '../python/data_analysis/main.py');
+        console.log('Cities:', cities);
 
-        // Run your Python script and send state, niche, and cities as arguments
-        const pythonProcess = spawn('python', [scriptPath, state, niche, JSON.stringify(cities)]);
+        const options = {
+            mode: 'text',
+            pythonOptions: ['-u'], // unbuffered output
+            pythonPath: '/usr/local/bin/python3',
+            scriptPath: './python/data_analysis',
+            args: [niche, state.name, JSON.stringify(cities)],
+        };
 
-        // Process and analyze the response data using LangChain and Python
+        PythonShell.run('main.py', options, async (err, results) => {
+            if (err) {
+                console.error("PythonShell error:", err);
+                res.status(500).json(err);
+            } else {
+                // Process the returned results
+                const processedData = JSON.parse(results[0]);
 
-        const processedData = [
-            // ... your processed data here
-        ];
+                // Store the search results in the MySQL database in the results table
+                // const createdResults = await Result.bulkCreate(processedData);
 
-        // Store the search results in the MySQL database in the results table
-        const createdResults = await Result.bulkCreate(processedData);
+                // Return the search results to the front end
+                res.status(201).json(processedData);
+            }
+        });
 
-        // Return the search results to the front end
-        res.status(201).json(createdResults);
     } catch (err) {
         res.status(500).json(err);
     }
